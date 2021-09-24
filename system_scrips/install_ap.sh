e7a491f876bcc2a4c55dbd29a2d432b65335e307
Rubén Abad <gecko386@gmail.com>
2021-09-25 01:14:19 +0200

    previous bash scripts now uses bash utils

--- system_scrips/install_ap.sh
+++ system_scrips/install_ap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-sudo apt install -y libgtk-3-dev build-essential gcc g++ pkg-config make hostapd
+
+source $(dirname $0)/utils.sh
+information "#### configure access point ####"
+
+sudo apt install -y libgtk-3-dev build-essential gcc g++ pkg-config make hostapd git
+exit_if_last_command_failed "Cannot install dependencies. Please, check logs"
 
 cd access_point
 git clone https://github.com/lakinduakash/linux-wifi-hotspot
 cd linux-wifi-hotspot
 make
 sudo make install
+exit_if_last_command_failed "Cannot install access point scripts. Please, check logs"
 make clean
 
 sudo cp ../access_point.service /etc/systemd/system/.
@@ -13,3 +19,8 @@ sudo cp ../create_ap.conf /etc/.
 
 sudo systemctl enable access_point
 sudo systemctl start access_point
+
+exit_if_last_command_failed "Cannot install access point services. Please, check logs"
+
+information "### Access point configured!!! ###"
+exit 0
--- system_scrips/set_default_runlevel.sh
+++ system_scrips/set_default_runlevel.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo systemctl set-default ${1-multi-user}.target
+source $(dirname $0)/utils.sh
+
+information "#### set default runlevel as non graphic ####"
+sudo systemctl set-default multi-user.target 
+
+exit_if_last_command_failed "Problems setting default runlevel. Please, check logs"
+exit 0
