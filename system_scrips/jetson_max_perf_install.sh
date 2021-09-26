aa24358616a9a36221b6164401789792b66e1f33
Rubén Abad <gecko386@gmail.com>
2021-09-26 16:02:44 +0200

    remove unnecessary code

--- system_scrips/jetson_max_perf_install.sh
+++ system_scrips/jetson_max_perf_install.sh
@@ -7,9 +7,4 @@ cd jetson-fan-ctl
 sudo ./install.sh
 exit_if_last_command_failed "Cannot install automatic fan management. Please, check logs"
 
-sudo systemctl enable automagic-fan
-exit_if_last_command_failed "Cannot enable automatic fan service Please, check logs"
-sudo systemctl start automagic-fan
-exit_if_last_command_failed "Cannot start automatic fan service Please, check logs"
-
 information "#### automatic fan management installed!!! ####"
