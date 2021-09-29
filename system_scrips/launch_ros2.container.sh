42ce324707b574a378c84aa90e6e4feba7a5118e
Rubén Abad <gecko386@gmail.com>
2021-09-30 01:44:23 +0200

    docker launch scripts xhost enabled and passing cis camera access to container

--- system_scrips/launch_ros2.container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source $(dirname $0)/utils.sh
-information "#### executing ROS2 Galactic container  ####"
-sudo docker run -it --rm --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w $HOME --mount src=/home,target=/home,type=bind ros:galactic-pytorch-l4t-r32.6.1 
