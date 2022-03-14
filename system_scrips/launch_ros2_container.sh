d10c5b9bd082e3190cbb61835af61d378e71e2a0
Ruben Abad <gecko386@gmail.com>
2022-03-14 23:58:33 +0100

    solve error executing xhost in script

--- system_scrips/launch_ros2_container.sh
+++ system_scrips/launch_ros2_container.sh
@@ -2,5 +2,5 @@
 
 source $(dirname $0)/utils.sh
 information "#### executing ROS2 Galactic container  ####"
-xhost+
+xhost +
 sudo docker run -it --rm --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/argus_socket:/tmp/argus_socket -e DISPLAY=$DISPLAY -w $HOME --mount src=/home,target=/home,type=bind ros:galactic-pytorch-l4t-r32.6.1 
