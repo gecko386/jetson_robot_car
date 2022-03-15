8b187b73cd7aec721c2c15287abaaf79b6e4ce3e
Ruben Abad <gecko386@gmail.com>
2022-03-15 02:06:13 +0100

    fix build of ultrasound publisher

--- src/distance_sensors/setup.py
+++ src/distance_sensors/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ultrasound_publisher = distance_sensors.ultrasound_publisher:main',
         ],
     },
 )
