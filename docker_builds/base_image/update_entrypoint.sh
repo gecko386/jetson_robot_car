d2d1857ff20698984c9043078a2531e694b348df
Ruben Abad <gecko386@gmail.com>
2022-03-15 01:14:44 +0100

    fix sed substitution

--- docker_builds/base_image/update_entrypoint.sh
+++ docker_builds/base_image/update_entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [[ ! -f "/docker_entrypoint.sh" ]];
+if [[  -f "/ros_entrypoint.sh" ]];
 then
-	touch /docker_entrypoint.sh
-	chmod 755 /docker_entrypoint.sh
-
-    echo '#!/bin/bash
-set -e
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-exec "$@"' > /docker_entrypoint.sh
-
+    sed -i 's/exec "$@"/source "\/home\/nvidia\/jetson_robot_car\/install\/local_setup.sh"\nexec "$@"/g' /ros_entrypoint.sh;
 fi
-
-sed -i 's/exec "$@"/source "\/home\/nvidia/\/jetson_robot_car\/install\/local_setup.sh"\nexec "$@"/g' /ros_entrypoint.sh
