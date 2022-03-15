1b80a61e6779cc697ce8c1795fa9735fb14331b3
Ruben Abad <gecko386@gmail.com>
2022-03-15 01:09:54 +0100

    fix bad sed substitution

--- docker_builds/base_image/update_entrypoint.sh
+++ docker_builds/base_image/update_entrypoint.sh
@@ -13,4 +13,4 @@ exec "$@"' > /docker_entrypoint.sh
 
 fi
 
-sed -i 's/exec "$@"/source "\/home\/nvidia/\jetson_robot_car\/install\/local_setup.sh"\nexec "$@"/g' /ros_entrypoint.sh
+sed -i 's/exec "$@"/source "\/home\/nvidia/\/jetson_robot_car\/install\/local_setup.sh"\nexec "$@"/g' /ros_entrypoint.sh
