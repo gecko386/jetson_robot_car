f107f8dd08ffceee72b54cd107caf3905af1e936
Ruben Abad <gecko386@gmail.com>
2022-03-16 00:28:19 +0100

    enable use of GPIO in docker

--- system_scrips/launch_ros2_container.sh
+++ system_scrips/launch_ros2_container.sh
@@ -3,4 +3,4 @@
 source $(dirname $0)/utils.sh
 information "#### executing ROS2 Galactic container  ####"
 xhost +
-sudo docker run -it --rm --gpus all --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/argus_socket:/tmp/argus_socket -e DISPLAY=$DISPLAY -w $HOME --mount src=/home,target=/home,type=bind base_image
+sudo docker run --privileged -it --rm --gpus all --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/argus_socket:/tmp/argus_socket -v /etc/udev/rules.d:/etc/udev/rules.d -v /dev:/dev -v /sys/class/gpio:/sys/class/gpio -v /sys/devices:/sys/devices -v /dev/gpiochip0:/dev/gpiochip0 -v /dev/gpiochip1:/dev/gpiochip1 -v /sys/firmware/devicetree/base:/sys/firmware/devicetree/base -e DISPLAY=$DISPLAY -w $HOME --mount src=/home,target=/home,type=bind base_image
