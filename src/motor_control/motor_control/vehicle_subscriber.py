2be6abb78ab328910315f66f86772c2605f902b7
Ruben Abad <gecko386@gmail.com>
2022-03-19 19:47:50 +0100

    reverting speed calculus and fixing contribs

--- src/motor_control/motor_control/vehicle_subscriber.py
+++ src/motor_control/motor_control/vehicle_subscriber.py
@@ -35,10 +35,10 @@ class VehicleSubscriber(Node):
         speed = ackermann_order.speed
         angle = ackermann_order.steering_angle
 
-        #m1, m2 = self._car.get_motor_speeds(speed*0.5, angle*0.5)
+        m1, m2 = self._car.get_motor_speeds(speed*0.7, angle*0.3)
 
-        m1 = speed * (math.cos(angle) - math.sin(angle))
-        m2 = speed * (math.sin(angle) + math.cos(angle))
+        #m1 = speed * (math.cos(angle) - math.sin(angle))
+        #m2 = speed * (math.sin(angle) + math.cos(angle))
 
         self.get_logger().info('Motor 1 speed:' + str(m1))
         self.get_logger().info('Motor 2 speed:' + str(m2))
