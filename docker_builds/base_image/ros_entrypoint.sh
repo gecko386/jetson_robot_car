3e3b93dc1960ca93ca3047b532ee8a5c07d87f70
Ruben Abad <gecko386@gmail.com>
2022-03-20 12:24:01 +0100

    fix ros source

--- docker_builds/base_image/ros_entrypoint.sh
+++ docker_builds/base_image/ros_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source "/opt/ros/galactic/setup.bash"
+source "/opt/ros/galactic/install/setup.bash"
 source "/opt/ros_realsense/install/local_setup.bash"
 source "/opt/ros_deep_learning/install/local_setup.bash"
 source "/home/nvidia/jetson_robot_car/install/local_setup.sh"
