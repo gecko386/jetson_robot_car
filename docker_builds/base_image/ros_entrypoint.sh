0bb4fc39cd709e932fa807712dea23f7a887a318
Ruben Abad <gecko386@gmail.com>
2022-03-20 19:27:52 +0100

    initial LIDAR support

--- docker_builds/base_image/ros_entrypoint.sh
+++ docker_builds/base_image/ros_entrypoint.sh
@@ -2,6 +2,7 @@
 
 source "/opt/ros/galactic/install/setup.bash"
 source "/opt/ros_realsense/install/local_setup.bash"
+source "/opt/ydlidar_ros2/install/local_setup.bash"
 source "/opt/ros_deep_learning/install/local_setup.bash"
 source "/home/nvidia/jetson_robot_car/install/local_setup.sh"
 exec $@
