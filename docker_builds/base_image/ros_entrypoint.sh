8a47c70c2813ead510d6bfed6e1bf85a8df0f54f
Ruben Abad <gecko386@gmail.com>
2022-03-20 11:48:01 +0100

    add build jetson-inference in base image

--- docker_builds/base_image/ros_entrypoint.sh
+++ docker_builds/base_image/ros_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source "/opt/ros_extras/install/local_setup.bash"
+source "/opt/ros/galactic/setup.bash"
+source "/opt/ros_realsense/install/local_setup.bash"
+source "/opt/ros_deep_learning/install/local_setup.bash"
 source "/home/nvidia/jetson_robot_car/install/local_setup.sh"
 exec $@
