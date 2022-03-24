e8ddadb59c4bd5cdad147f9326857da34192ba83
Ruben Abad <gecko386@gmail.com>
2022-03-25 00:06:41 +0100

    try another container

--- system_scrips/create_docker_container.sh
+++ system_scrips/create_docker_container.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/utils.sh
 
 go_to_folder_or_die /home/nvidia/jetson_robot_car/docker_builds/base_image
 cp /proc/device-tree/compatible compatible
-sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="dustynv/ros:galactic-pytorch-l4t-r32.5.0" -f Dockerfile .
+sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="dustynv/ros:galactic-pytorch-l4t-r32.7.1" -f Dockerfile .
 
 exit_if_last_command_failed "Cannot create container. Please, check logs"
 
