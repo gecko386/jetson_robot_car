135b203fc6b096db9028e42b471ac9b4e7bfaa9b
Ruben Abad <gecko386@gmail.com>
2022-03-25 23:47:02 +0100

    clean artifacts before build

--- system_scrips/build_packages.sh
+++ system_scrips/build_packages.sh
@@ -7,6 +7,10 @@ information "### build ros2 components inside a docker container ###"
 
 git clone -b ros2 https://github.com/ros-drivers/ackermann_msgs.git /home/nvidia/jetson_robot_car/ros_pkgs/src/ackermann_msgs
 
+sudo rm -fr /home/nvidia/jetson_robot_car/ros_pkgs/build
+sudo rm -fr /home/nvidia/jetson_robot_car/ros_pkgs/log
+sudo rm -fr /home/nvidia/jetson_robot_car/ros_pkgs/install
+
 sudo docker run -it --rm -v /home:/home -w /home/nvidia/jetson_robot_car/ros_pkgs base_image colcon build --symlink-install
 exit_if_last_command_failed "Cannot build ros2 components. Please, check logs"
 
