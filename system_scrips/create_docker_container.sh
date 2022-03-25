a0c0dbdc47b251e56f27303cccb7018714cae42a
Ruben Abad <gecko386@gmail.com>
2022-03-25 20:24:02 +0100

    download images instead of building from zero

--- system_scrips/create_docker_container.sh
+++ system_scrips/create_docker_container.sh
@@ -7,17 +7,17 @@ sudo cp docker-config/daemon.json /etc/docker/.
 sudo systemctl restart docker.service
 exit_if_last_command_failed "Cannot configure docker. Please, check logs"
 
-information "### Create docker container with ROS2 Galactic and pytorch ###"
-git clone https://github.com/dusty-nv/jetson-containers.git
+#information "### Create docker container with ROS2 Galactic and pytorch ###"
+#git clone https://github.com/dusty-nv/jetson-containers.git
 
-exit_if_last_command_failed "Cannot download utils. Please, check logs"
-cd jetson-containers
-./scripts/docker_build_ros.sh --distro galactic  --with-pytorch
-exit_if_last_command_failed "Cannot create container. Please, check logs"
+#exit_if_last_command_failed "Cannot download utils. Please, check logs"
+#cd jetson-containers
+#./scripts/docker_build_ros.sh --distro galactic  --with-pytorch
+#exit_if_last_command_failed "Cannot create container. Please, check logs"
 
 go_to_folder_or_die /home/nvidia/jetson_robot_car/docker_builds/base_image
 cp /proc/device-tree/compatible compatible
-sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="ros:galactic-pytorch-l4t-r32.7.1" -f Dockerfile .
+sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="dustynv/ros:galactic-pytorch-l4t-r32.7.1" -f Dockerfile .
 
 exit_if_last_command_failed "Cannot create container. Please, check logs"
 
