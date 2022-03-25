d88b18d7c1eb421c870d39876ebbf9727d7114f6
Ruben Abad <gecko386@gmail.com>
2022-03-25 23:44:29 +0100

    add installation of ackermann messages

--- system_scrips/build_packages.sh
+++ system_scrips/build_packages.sh
@@ -4,6 +4,9 @@ source $(dirname $0)/utils.sh
 
 information "### build ros2 components inside a docker container ###"
 
+
+git clone -b ros2 https://github.com/ros-drivers/ackermann_msgs.git /home/nvidia/jetson_robot_car/ros_pkgs/src/ackermann_msgs
+
 sudo docker run -it --rm -v /home:/home -w /home/nvidia/jetson_robot_car/ros_pkgs base_image colcon build --symlink-install
 exit_if_last_command_failed "Cannot build ros2 components. Please, check logs"
 
