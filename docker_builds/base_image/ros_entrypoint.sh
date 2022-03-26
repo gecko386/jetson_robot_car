10ce3ed2674ec78d214dcde5202a01f5fda3cd70
Ruben Abad <gecko386@gmail.com>
2022-03-26 02:06:54 +0100

    fix ros deep learnig package build

--- docker_builds/base_image/ros_entrypoint.sh
+++ docker_builds/base_image/ros_entrypoint.sh
@@ -3,6 +3,6 @@
 source "/opt/ros/galactic/install/setup.bash"
 source "/opt/ros_realsense/install/local_setup.bash"
 source "/opt/ydlidar_ros2/install/local_setup.bash"
-source "/opt/ros_deep_learning/install/local_setup.bash"
+source "/opt/ros_dl/install/local_setup.bash"
 source "/home/nvidia/jetson_robot_car/ros_pkgs/install/local_setup.sh"
 exec $@
