6ecf5f6e36f7513c48402164185be49feb373abd
Ruben Abad <gecko386@gmail.com>
2022-03-28 01:55:17 +0200

    change docker dusty base image

--- system_scrips/create_docker_container.sh
+++ system_scrips/create_docker_container.sh
@@ -19,7 +19,7 @@ exit_if_last_command_failed "Cannot configure docker. Please, check logs"
 
 go_to_folder_or_die /home/nvidia/jetson_robot_car/docker_builds/base_image
 cp /proc/device-tree/compatible compatible
-sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="dustynv/ros:galactic-pytorch-l4t-r32.7.1" -f Dockerfile .
+sudo docker build --rm -t "base_image" --build-arg BASE_IMAGE="dustynv/ros:galactic-pytorch-l4t-r32.6.1" -f Dockerfile .
 
 exit_if_last_command_failed "Cannot create container. Please, check logs"
 
